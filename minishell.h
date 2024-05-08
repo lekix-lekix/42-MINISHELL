fe694ef45616b5eb7d3d77b26b0b0752edc9e999
Kilian Pouliquen <kipouliq@paul-f5Ar1s1.clusters.42paris.fr>
2024-05-08 18:25:32 +0200

    worked on converting env into a linked list. added a garbage collector that needs testing

--- minishell.h
+++ minishell.h
@@ -6,30 +6,51 @@
 /*   By: kipouliq <kipouliq@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/07 16:26:11 by kipouliq          #+#    #+#             */
-/*   Updated: 2024/05/07 18:27:46 by kipouliq         ###   ########.fr       */
+/*   Updated: 2024/05/08 18:06:41 by kipouliq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
+# include "./42-MEGALIBFT/megalibft.h"
 # include <readline/history.h>
 # include <readline/readline.h>
 # include <stdio.h>
 # include <stdlib.h>
-# include "./42-MEGALIBFT/megalibft.h"
-
 
 typedef struct s_env
 {
-    char    *field;
-    char    *content;
-}           t_env;
+	char			*field;
+	char			*content;
+	struct s_env	*next;
+}					t_env;
 
 typedef struct s_minishell
 {
-    t_env   *env_lst;
-	char	*path;
-}			t_minishell;
+	t_env			*env_lst;
+	char			*path;
+}					t_minishell;
+
+typedef struct s_gbg
+{
+	t_lst			*mlc_env;
+	t_lst			*mlc_parsing;
+}					t_gbg;
+
+typedef enum e_gbg_rules
+{
+	ADD,
+	FREE,
+	FLUSH_ONE,
+    FLUSH_ALL
+}					t_gbg_rules;
+
+typedef enum e_gbg_lst_names
+{
+    ENV,
+    PARSING,
+    ALL
+}                   t_gbg_lst_names;
 
 #endif
--- srcs/minishell.c
+++ srcs/minishell.c
@@ -6,7 +6,7 @@
 /*   By: kipouliq <kipouliq@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/07 16:27:00 by kipouliq          #+#    #+#             */
-/*   Updated: 2024/05/07 18:28:16 by kipouliq         ###   ########.fr       */
+/*   Updated: 2024/05/08 18:23:35 by kipouliq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,55 +30,251 @@ char	*get_path(char **envp)
 	return (str);
 }
 
-int     ft_strlen_sep(char *str, char c)
+int	free_env_lst(t_env **lst)
 {
-    int i;
+	t_env	*node;
+	t_env	*next;
 
-    i = 0;
-    while (str[i] && str[i] != c)
-        i++;
-    return (i);
+	node = *lst;
+	while (node)
+	{
+		next = node->next;
+		free(node->content);
+		free(node->field);
+		free(node);
+		node = next;
+	}
+	return (0);
 }
 
-// t_env	*get_env_lst(char **envp)
-// {
-// 	t_env	*lst;
-// 	t_env	*new_node;
-//     char    *sep;
-// 	int		i;
+int	ft_strlen_sep(char *str, char *sep)
+{
+	int	i;
 
-//     new_node = malloc(sizeof(t_env));
-//     if (!new_node)
-//         return (NULL);
-//     i = -1;
-//     while (envp && envp[++i])
-//     {
-//         sep = ft_strchr(envp[i], '=');
-//         new_node->content = malloc(sizeof(char) * (ft_strlen_sep(envp[i], '=')) + 1);
-//         if (!new_node->content)
-//             return (NULL);
-//         new_node->field = malloc(sizeof(char) * (ft_strlen_sep(envp[i], '\0')) + 1);
-//         if (!new_node->field)
-//             return (NULL);
-//     }
-// }
+	i = 0;
+	while (str[i] && str[i] != *sep)
+		i++;
+	return (i + 1);
+}
+
+void	lst_env_add_back(t_env **lst, t_env *new)
+{
+	t_env	*list;
+
+	list = *lst;
+	while (list->next)
+		list = list->next;
+	if (!new)
+		list->next = NULL;
+	else
+		list->next = new;
+}
+
+t_env	*create_env_node(char *str, char *sep)
+{
+	t_env	*new_node;
+
+	new_node = malloc(sizeof(t_env));
+	if (!new_node)
+		return (NULL);
+	new_node->field = malloc(sizeof(char) * (ft_strlen_sep(str, sep) + 1));
+	if (!new_node->field)
+		return (free(new_node), NULL);
+	ft_strlcpy(new_node->field, str, ft_strlen_sep(str, sep) + 1);
+	new_node->content = malloc(sizeof(char) * (ft_strlen(sep + 1) + 1));
+	if (!new_node->content)
+		return (free(new_node->content), free(new_node), NULL);
+	ft_strlcpy(new_node->content, sep + 1, ft_strlen(sep));
+	new_node->next = NULL;
+	return (new_node);
+}
+
+t_env	*get_env_lst(char **envp)
+{
+	t_env	*lst;
+	t_env	*node;
+	char	*sep;
+	int		i;
+
+	i = -1;
+	while (envp && envp[++i])
+	{
+		sep = ft_strchr(envp[i], '=');
+		node = create_env_node(envp[i], sep);
+		if (!node)
+			return (free_env_lst(&lst), NULL); // free lst
+		if (i == 0)
+		{
+			lst = node;
+			continue ;
+		}
+		else
+			lst_env_add_back(&lst, node);
+	}
+	return (lst);
+}
 
 int	init_data(t_minishell *data, char **envp)
 {
 	data->path = get_path(envp);
 	if (!data->path)
 		return (-1);
-	// data->env_lst = get_env_lst(envp);
+	data->env_lst = get_env_lst(envp);
+	if (!data->env_lst)
+		return (-1);
 	return (0);
 }
 
-int	print_env(char **envp)
+int	print_env(t_env **lst)
+{
+	t_env	*node;
+
+	node = *lst;
+	while (node)
+	{
+		printf("field = %s\n", node->field);
+		printf("content = %s\n", node->content);
+		node = node->next;
+	}
+	return (0);
+}
+
+int	print_real_env(char **env)
 {
 	int	i;
 
 	i = -1;
-	while (envp && envp[++i])
-		printf("%s\n", envp[i]);
+	while (env[++i])
+		printf("%s\n", env[i]);
+	return (0);
+}
+
+int	env_lst_size(t_env **lst)
+{
+	t_env	*node;
+	int		size;
+
+	node = *lst;
+	size = 0;
+	while (node)
+	{
+		size++;
+		node = node->next;
+	}
+	return (size);
+}
+
+int	free_tab_index(char **tab, int i)
+{
+	while (i >= 0)
+	{
+		free(tab[i]);
+		i--;
+	}
+	free(tab);
+	return (0);
+}
+
+char	**env_lst_to_arr(t_env **lst)
+{
+	char	**env_arr;
+	t_env	*node;
+	int		i;
+
+	node = *lst;
+	env_arr = malloc(sizeof(char *) * (env_lst_size(lst) + 1));
+	if (!env_arr)
+		return (NULL);
+	i = 0;
+	while (node)
+	{
+		env_arr[i] = ft_strjoin(node->field, node->content);
+		if (!env_arr[i])
+			return (free_tab_index(env_arr, i), NULL);
+		node = node->next;
+		i++;
+	}
+	env_arr[i] = NULL;
+	return (env_arr);
+}
+
+// int	main(int argc, char **argv, char **envp)
+// {
+// 	(void)argc;
+// 	(void)argv;
+// 	char *prompt;
+// 	t_minishell data;
+
+// 	if (init_data(&data, envp) == -1)
+// 		return (-1);
+// 	prompt = NULL;
+// 	while (1)
+// 	{
+// 		prompt = readline("./minishell>");
+// 		free(prompt);
+// 	}
+// 	free(data.path);
+// }
+
+t_lst	*ft_find_node(t_lst **list, void *mem_addr)
+{
+	t_lst	*node;
+
+	node = *list;
+	while (node)
+	{
+		if (node->content == mem_addr)
+			return (node);
+		node = node->next;
+	}
+	return (NULL);
+}
+
+void	add_node_gbg(int which_lst, t_gbg *all_lsts, t_lst *node)
+{
+	if (which_lst == ENV)
+		ft_lstadd_back(&all_lsts->mlc_env, node);
+	if (which_lst == PARSING)
+		ft_lstadd_back(&all_lsts->mlc_parsing, node);
+}
+
+void	remove_free_mem_node(int which_lst, t_gbg *all_lsts, void *mem_addr)
+{
+	t_lst	*to_remove;
+
+	if (which_lst == ENV)
+		to_remove = ft_find_node(&all_lsts->mlc_env, mem_addr);
+	if (which_lst == PARSING)
+		to_remove = ft_find_node(&all_lsts->mlc_parsing, mem_addr);
+	free(to_remove->content);
+	free(to_remove);
+}
+
+void	flush_lst(t_gbg *all_lsts, int which_list)
+{
+	if (which_list == ENV)
+		ft_lstclear(&all_lsts->mlc_env, free);
+	if (which_list == PARSING)
+		ft_lstclear(&all_lsts->mlc_parsing, free);
+}
+
+int	garbage_collector(void *mem_addr, int which_list, int rule)
+{
+	static t_gbg	*all_gbg_lsts;
+	t_lst			*node;
+
+	node = NULL;
+	if (rule == ADD)
+	{
+		node = ft_lstnew(mem_addr);
+		// if (!node) // to do
+		// return (garbage_collector(NULL, FLUSH), -1);
+		add_node_gbg(which_list, all_gbg_lsts, node);
+	}
+	if (rule == FREE)
+		remove_free_mem_node(which_list, all_gbg_lsts, mem_addr);
+	if (rule == FLUSH_ONE)
+		flush_lst(all_gbg_lsts, which_list);
 	return (0);
 }
 
@@ -86,16 +282,16 @@ int	main(int argc, char **argv, char **envp)
 {
 	(void)argc;
 	(void)argv;
-	char *prompt;
-	t_minishell data;
+	t_env *lst;
+	char **env;
+	int i = -1;
 
-	if (init_data(&data, envp) == -1)
-		return (-1);
-	prompt = NULL;
-	while (1)
-	{
-		prompt = readline("minishell>");
-		free(prompt);
-	}
-	free(data.path);
+	lst = get_env_lst(envp);
+	print_real_env(envp);
+	print_env(&lst);
+	env = env_lst_to_arr(&lst);
+	free_env_lst(&lst);
+	while (env[++i])
+		free(env[i]);
+	free(env);
 }
