0fb2a76fd1216ee8b47d6319292f38df4e47f5db
Kilian Pouliquen <kipouliq@made-f0Br6s7.clusters.42paris.fr>
2024-05-23 19:23:56 +0200

    thats a working ast

--- srcs/tokenizer.c
+++ srcs/tokenizer.c
@@ -6,7 +6,7 @@
 /*   By: kipouliq <kipouliq@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 13:45:25 by kipouliq          #+#    #+#             */
-/*   Updated: 2024/05/16 18:34:26 by kipouliq         ###   ########.fr       */
+/*   Updated: 2024/05/22 18:25:21 by kipouliq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,23 @@ int	check_operator_len(char *str, int *op_len)
 		return (0);
 	*op_len = i;
 	return (1);
+}
+
+void	find_operator_type(char *input, t_token *node)
+{  
+    int	op_len;
+
+	check_operator_len(input, &op_len);
+	if (*input == '(')
+		node->type = PAR_LEFT;
+	else if (*input == ')')
+		node->type = PAR_RIGHT;
+	else if (*input == '|' && op_len == 1)
+		node->type = PIPE;
+	else if (*input == '|' && op_len == 2)
+		node->type = OR;
+	else if (*input == '&' && op_len == 2)
+		node->type = AND;
 }
 
 t_token	*create_operator_node(char **input)
@@ -42,9 +59,8 @@ t_token	*create_operator_node(char **input)
 	if (!node->content || gbg_coll(node->content, PARSING, ADD))
 		return (gbg_coll(NULL, ALL, FLUSH_ALL), exit(255), NULL);
 	ft_strlcpy(node->content, input_copy, operator_len + 1);
-	node->type = OPERATOR;
-	node->left = NULL;
-	node->right = NULL;
+	find_operator_type(input_copy, node);
+	node->next = NULL;
 	*input += operator_len;
 	return (node);
 }
@@ -63,36 +79,96 @@ t_token	*create_cmd_node(char *input, char *sep)
 		return (gbg_coll(NULL, ALL, FLUSH_ALL), exit(255), NULL);
 	ft_strcpy_sep(node->content, input, sep);
 	node->type = CMD;
-	node->left = NULL;
-	node->right = NULL;
+	node->next = NULL;
 	return (node);
+}
+
+// t_token	*tokenize_input(char *input)
+// {
+// 	t_token	*root;
+// 	t_token	*cmd_node;
+// 	t_token	*op_node;
+// 	char	*input_parse;
+// 	char	*operator;
+
+// 	root = NULL;
+// 	input_parse = input;
+// 	while (input_parse)
+// 	{
+// 		operator = find_operator(input_parse);
+// 		if (!operator)
+// 		{
+// 			cmd_node = create_cmd_node(input_parse, NULL);
+// 			insert_cmd_node(&root, cmd_node);
+// 			break ;
+// 		}
+// 		op_node = create_operator_node(&operator);
+// 		if (!op_node)
+// 			return (NULL);
+// 		cmd_node = create_cmd_node(input_parse, op_node->content);
+// 		insert_operator_node(&root, op_node);
+// 		insert_cmd_node(&root, cmd_node);
+// 		input_parse = operator;
+// 	}
+// 	return (root);
+// }
+
+void	insert_node_lst(t_token **lst, t_token *node)
+{
+	t_token	*root;
+
+	root = *lst;
+    if (!root)
+    {
+        *lst = node;
+        return ;
+    }
+	while (root->next)
+		root = root->next;
+	root->next = node;
+}
+
+int ft_is_space(char c)
+{
+    return ((c >= 9 && c <= 13) || c == 32);
+}
+
+char *skip_spaces(char *str)
+{
+    int i;
+
+    i = 0;
+    while (str[i] && ft_is_space(str[i]))
+        i++;
+    return (str + i);
 }
 
 t_token	*tokenize_input(char *input)
 {
-	t_token	*root;
-	t_token	*cmd_node;
-	t_token	*op_node;
-	char	*input_parse;
-	char	*operator;
+	t_token *root;
+	t_token *op_node;
+	t_token *cmd_node;
+	char *input_parse;
+	char *operator;
 
 	root = NULL;
 	input_parse = input;
-	while (input_parse)
+	while (1)
 	{
 		operator = find_operator(input_parse);
 		if (!operator)
 		{
 			cmd_node = create_cmd_node(input_parse, NULL);
-			insert_cmd_node(&root, cmd_node);
+            insert_node_lst(&root, cmd_node);
 			break ;
 		}
 		op_node = create_operator_node(&operator);
 		if (!op_node)
 			return (NULL);
+        input_parse = skip_spaces(input_parse);
 		cmd_node = create_cmd_node(input_parse, op_node->content);
-		insert_operator_node(&root, op_node);
-		insert_cmd_node(&root, cmd_node);
+		insert_node_lst(&root, cmd_node);
+		insert_node_lst(&root, op_node);
 		input_parse = operator;
 	}
 	return (root);
