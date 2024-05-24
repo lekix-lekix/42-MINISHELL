e31a6608d668334fdf4fbef0fab186caec1b3751
Kilian Pouliquen <kipouliq@paul-f5Ar2s2.clusters.42paris.fr>
2024-05-24 15:45:32 +0200

    ast is working, merging into main

--- srcs/ast_tools.c
+++ srcs/ast_tools.c
@@ -6,35 +6,11 @@
 /*   By: kipouliq <kipouliq@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 13:53:14 by kipouliq          #+#    #+#             */
-/*   Updated: 2024/05/22 14:43:02 by kipouliq         ###   ########.fr       */
+/*   Updated: 2024/05/24 15:44:28 by kipouliq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
-
-// void	parse_clear_tree(t_token *root)
-// {
-// 	if (!root)
-// 		return ;
-// 	if (root->left)
-// 		parse_clear_tree(root->left);
-// 	if (root->right)
-// 		parse_clear_tree(root->right);
-// 	if (root->content)
-// 		gbg_coll(root->content, PARSING, FREE);
-// 	if (root)
-// 		gbg_coll(root, PARSING, FREE);
-// }
-
-// void	clear_tree(t_token **root)
-// {
-// 	t_token	*node;
-
-// 	if (!root || !*root)
-// 		return ;
-// 	node = *root;
-// 	parse_clear_tree(node);
-// }
 
 int	ft_strlen_sep(char *str, char *sep)
 {
